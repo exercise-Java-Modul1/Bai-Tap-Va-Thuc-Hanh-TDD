b87da68ae04cb8097249682bb510d272bb078d2c
1234567890 <phantiensy195@gmail.com>
2018-06-24 09:04:41 +0700

    AbsoluteNumberCalculator

---
+++ AbsoluteNumberCalculator.java
@@ -0,0 +1,8 @@
+public class AbsoluteNumberCalculator {
+    static int findAbsolute(int number) {
+        if (number < 0) {
+            return -number;
+        }
+        return number;
+    }
+}
